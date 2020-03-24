bd0776df9f811f8259b954be889de85d04150bb5
Cowan <ctcumming@gmail.com>
2020-03-23 23:24:25 -0700

    Added distance from stranger rule.

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from classroom import Classroom
 from person import Person
-from rules import Rule, FrontOfClassRule
+from rules import Rule, FrontOfClassRule, FarFromStrangersRule
 from util import Position
 
 if __name__ == '__main__':
     classroom = Classroom(20, 10)
 
-    for i in range(100):
-        person = Person(Position(10, 5), [FrontOfClassRule(0.5)])
+    for i in range(20):
+        person = Person(Position(10, 5), [FrontOfClassRule(0), FarFromStrangersRule(1, neighbourhood=3)])
         person.sit_down(classroom)
 
     print(classroom._layout)
--- rules.py
+++ rules.py
@@ -1,3 +1,5 @@
+import math
+
 from classroom import Classroom
 from util import Position
 
@@ -9,13 +11,39 @@ class Rule:
 
 
 class FrontOfClassRule(Rule):
-    weight: float
+    _weight: float
 
     def __init__(self, weight: float):
         assert weight >= 0
 
-        self.weight = weight
+        self._weight = weight
 
     def calculate_cost(self, person_position: Position, seat_position: Position, classroom: Classroom) -> float:
-        return seat_position.y * self.weight
+        return seat_position.y * self._weight
 
+
+class FarFromStrangersRule(Rule):
+    _weight: float
+    _neighbourhood: int
+
+    def __init__(self, weight: float, neighbourhood: int = 1):
+        self._weight = weight
+        self._neighbourhood = neighbourhood
+
+    def calculate_cost(self, person_position: Position, seat_position: Position, classroom: Classroom) -> float:
+        cost = 0
+
+        for i in range(-self._neighbourhood, self._neighbourhood + 1):
+            for j in range(-self._neighbourhood, self._neighbourhood + 1):
+                if i == 0 and j == 0:
+                    continue
+
+                x = seat_position.x + i
+                y = seat_position.y + j
+                if x < 0 or x >= classroom.width or y < 0 or y >= classroom.length:
+                    continue
+
+                if classroom.get_seat(Position(x, y)):
+                    cost += self._weight / math.sqrt(i ** 2 + j ** 2)
+
+        return cost
